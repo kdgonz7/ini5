be97f19d9e36389012dd4c65de29cdb1c35283aa
Kai D. Gonzalez <gkai70263@gmail.com>
2024-10-04 08:04:00 -0400

    reorganize build script

--- build.zig
+++ build.zig
@@ -5,14 +5,11 @@ pub fn build(b: *std.Build) !void {
     const optimize_options = b.standardOptimizeOption(.{
         .preferred_optimize_mode = .ReleaseSafe,
     });
+
     const ini5_tests = b.addTest(.{
         .root_source_file = b.path("lib/INI5.zig"),
     });
 
-    const test_step = b.step("tests", "Run unit tests for INI5. They should all be passing.");
-    test_step.dependOn(&b.addRunArtifact(ini5_tests).step);
-
-    const build_step = b.step("visitor", "Build the Ini visitor program (to print INI section results)");
     const ini5_visitor = b.addExecutable(.{
         .name = "ini-visitor",
         .target = target_options,
@@ -21,7 +18,15 @@ pub fn build(b: *std.Build) !void {
         .link_libc = true,
     });
 
-    build_step.dependOn(&b.addInstallArtifact(ini5_visitor, .{
+    var inivisitor_install_artifact = b.addInstallArtifact(ini5_visitor, .{
         .dest_sub_path = "inivisitor",
-    }).step);
+    });
+
+    var ini5_tests_artifact = b.addRunArtifact(ini5_tests);
+
+    const test_step = b.step("tests", "Run unit tests for INI5. They should all be passing.");
+    const build_step = b.step("visitor", "Build the Ini visitor program (to print INI section results)");
+
+    test_step.dependOn(&ini5_tests_artifact.step);
+    build_step.dependOn(&inivisitor_install_artifact.step);
 }
