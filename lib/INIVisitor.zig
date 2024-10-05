9ea5b49f82c37dea9cc39758d3cad3b715d599ed
Kai D. Gonzalez <gkai70263@gmail.com>
2024-10-04 20:46:08 -0400

    add colors for INIVisitor

--- lib/INIVisitor.zig
+++ lib/INIVisitor.zig
@@ -6,6 +6,49 @@ const INIValue = INI5.INIValue;
 const INIValueType = INI5.INIValueType;
 
 const parseFile = INI5.parseFile;
+pub const std_options: std.Options = .{
+    .logFn = log,
+};
+
+pub fn log(
+    comptime message_level: std.log.Level,
+    comptime _: @Type(.enum_literal),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    switch (message_level) {
+        .info => {
+            const stdout = std.io.getStdOut().writer();
+            _ = stdout.write("\x1b[;1mInfo: ") catch unreachable;
+            _ = stdout.write("\x1b[0m") catch unreachable;
+            stdout.print(format, args) catch unreachable;
+            _ = stdout.write("\n") catch unreachable;
+        },
+
+        .err => {
+            const stdout = std.io.getStdErr().writer();
+            _ = stdout.write("\x1b[;1mError: ") catch unreachable;
+            _ = stdout.write("\x1b[0m") catch unreachable;
+            stdout.print(format, args) catch unreachable;
+            _ = stdout.write("\n") catch unreachable;
+        },
+
+        .warn => {
+            const stdout = std.io.getStdErr().writer();
+            _ = stdout.write("\x1b[;1mWarning: ") catch unreachable;
+            _ = stdout.write("\x1b[0m") catch unreachable;
+            stdout.print(format, args) catch unreachable;
+            _ = stdout.write("\n") catch unreachable;
+        },
+        .debug => {
+            const stdout = std.io.getStdErr().writer();
+            _ = stdout.write("\x1b[;1mDebug: ") catch unreachable;
+            _ = stdout.write("\x1b[0m") catch unreachable;
+            stdout.print(format, args) catch unreachable;
+            _ = stdout.write("\n") catch unreachable;
+        },
+    }
+}
 
 pub fn visitSection(name: []const u8, section: INISection, indent: usize) void {
     for (0..indent) |_| {
